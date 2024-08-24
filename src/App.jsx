88456bcd3656ff2503bd9a19362386a1314b24dc
Kamran Ali <km775.ali@gmail.com>
2024-08-24 20:08:07 +0500

    paragraph added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
     <h1>hello word</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo fugit accusantium, reprehenderit quaerat vitae facere? Mollitia tenetur dignissimos minus repellat libero sed voluptatum reprehenderit voluptatem, laboriosam doloribus amet est deleniti.</p>
     </>
   )
 }
